ad114926c3666df25bc9630e003d98a5f4296028
Matthew Sullivan <msull92@gmail.com>
2014-02-21 13:40:02 -0600

    update gemspec, remove scaffold css, and add a model test for Post

--- test/models/blogelator/post_test.rb
+++ test/models/blogelator/post_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 module Blogelator
   class PostTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    it "orders by created at" do
+      hello = Post.create!(title: "Hello, world!", body_html: "Hello, world!", body_markdown: "Hello, world!")
+      world = Post.create!(title: "World hello!", body_html: "World hello!", body_markdown: "World hello!")
+
+      expect(User.ordered_by_created_at).to eq([hello, world])
+    end
   end
 end
