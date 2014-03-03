19cc3a0bf2ce9d1f4cadfbfa08be277ec2484a74
Brian Pattison <brian@brianpattison.com>
2014-03-02 22:34:27 -0600

    Use default travis script

--- app/helpers/blogelator/admin/posts_helper.rb
+++ app/helpers/blogelator/admin/posts_helper.rb
@@ -2,11 +2,6 @@ module Blogelator
   module Admin
     module PostsHelper
       include Blogelator::PostsHelper
-    
-      def site_title
-        "Blogelator"
-      end
-      
     end
   end
 end
