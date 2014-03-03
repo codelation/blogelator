5dbf2773d52088f91d91b1286ead48eb30290da2
Brian Pattison <brian@brianpattison.com>
2014-03-02 22:05:10 -0600

    Fix duplication in post helpers

--- app/helpers/blogelator/admin/posts_helper.rb
+++ app/helpers/blogelator/admin/posts_helper.rb
@@ -1,30 +1,12 @@
 module Blogelator
   module Admin
     module PostsHelper
-      include LocalTimeHelper
-      
-      def body_class
-        @body_class ||= "default"
-        body_class = "blogelator"
-        body_class += " " + controller.controller_name
-        body_class += " " + controller.action_name
-        body_class += " " + @body_class
-        body_class
-      end
-    
-      def page_title
-        page = @title
-        if page
-          "#{page} • #{site_title}"
-        else
-          site_title
-        end
-      end
+      include Blogelator::PostsHelper
     
       def site_title
         "Blogelator"
       end
-    
+      
     end
   end
 end
