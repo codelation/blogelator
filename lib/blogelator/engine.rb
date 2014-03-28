3f633cfd00ec171f29305e2a73941be8698947e6
Brian Pattison <brian@brianpattison.com>
2014-03-28 16:41:24 -0500

    Remove Font Awesome gem dependency

--- lib/blogelator/engine.rb
+++ lib/blogelator/engine.rb
@@ -1,12 +1,16 @@
 module Blogelator
   class Engine < ::Rails::Engine
     isolate_namespace Blogelator
-    
+
     # Precompile Blogelator Assets
     initializer "blogelator.assets.precompile" do |app|
-      app.config.assets.precompile += %w(blogelator/application.css blogelator/admin.css blogelator/admin.js)
+      app.config.assets.precompile += %w(
+        blogelator/application.css blogelator/admin.css blogelator/admin.js
+        blogelator/fontawesome-webfont.eot blogelator/fontawesome-webfont.svg
+        blogelator/fontawesome-webfont.ttf blogelator/fontawesome-webfont.woff
+      )
     end
-    
+
     # Add Blogelator Handlebars Templates Path
     initializer "blogelator.handlebars.config" do |app|
       unless app.config.handlebars.templates_root.is_a?(Array)
@@ -18,12 +22,12 @@ module Blogelator
         app.config.handlebars.templates_root << "blogelator/templates"
       end
     end
-    
+
     # Load Rails Generators
     generators do
       require File.join(File.expand_path("../..", __FILE__), "generators", "blogelator", "install_generator.rb")
     end
-    
+
     # RSpec Configuration
     config.generators do |g|
       g.test_framework :rspec, fixture: false
